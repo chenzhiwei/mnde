0424b32394f4e44337298a843ec8872abd0c7231
Chen Zhiwei <zhiweik@gmail.com>
2016-05-31 08:59:42 +0800

    enhance apply.sh

--- apply.sh
+++ apply.sh
@@ -2,4 +2,16 @@
 
 base=$(cd $(dirname apply.sh) && pwd)
 
-rsync -a "$base"/.[a-z0-9]* --exclude='.git' --exclude='.git[im]*' ~
+rsync -a "$base"/.[a-z0-9]* --exclude='.git' --exclude='.gitignore' --exclude='.gitmodules' $HOME
+
+if [ -f $HOME/.bashrc ]; then
+    if ! grep -wq bash_improve $HOME/.bashrc; then
+        echo '. $HOME/.bash_improve' >> $HOME/.bashrc
+    fi
+else
+    if ! grep -wq bash_improve $HOME/.bash_profile; then
+        echo '. $HOME/.bash_improve' >> $HOME/.bash_profile
+    fi
+fi
+
+# [ -f ~/.bashrc ] && echo '. $HOME/.bash_improve' >> ~/.bashrc || echo '. $HOME/.bash_improve' >> ~/.bash_profile
