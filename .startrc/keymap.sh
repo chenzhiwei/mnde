c227a35eb8282d48f5f39ea39f16583686800cd0
Chen Zhiwei <zhiweik@gmail.com>
2018-10-02 21:07:18 +0800

    backup gnome-terminal

---
+++ .startrc/keymap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xmodmap -e "keycode 76 = Insert Insert"
--- apply.sh
+++ apply.sh
@@ -4,6 +4,9 @@ base=$(cd $(dirname $0) && pwd)
 
 rsync -a "$base"/.[a-z0-9]* --exclude='.git' --exclude='.gitignore' --exclude='.gitmodules' $HOME
 
+# dconf dump /org/gnome/terminal/ > gnome-terminal.dconf
+dconf load /org/gnome/terminal/ < $base/gnome-terminal.dconf
+
 if [ -f $HOME/.bashrc ]; then
     if ! grep -wq bash_improve $HOME/.bashrc; then
         echo '. $HOME/.bash_improve' >> $HOME/.bashrc
