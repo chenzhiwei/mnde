fca44292f7e9ce4350f835248b1b34520fe208da
Chen Zhiwei <zhiweik@gmail.com>
2018-10-05 20:17:52 +0800

    update dconf

--- .config/zhiwei/deepin-screenshot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-QT_AUTO_SCREEN_SCALE_FACTOR=1 deepin-screenshot
--- apply.sh
+++ apply.sh
@@ -5,7 +5,8 @@ base=$(cd $(dirname $0) && pwd)
 rsync -a "$base"/.[a-z0-9]* --exclude='.git' --exclude='.gitignore' --exclude='.gitmodules' $HOME
 
 # dconf dump /org/gnome/terminal/ > gnome-terminal.dconf
-dconf load /org/gnome/terminal/ < $base/gnome-terminal.dconf
+dconf load /org/gnome/terminal/ < $base/dconf/gnome-terminal.dconf
+dconf load /org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/ < $base/dconf/custom-keybinding.dconf
 
 if [ -f $HOME/.bashrc ]; then
     if ! grep -wq bash_improve $HOME/.bashrc; then
