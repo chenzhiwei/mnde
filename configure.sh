8c424e34aacae9606c3a2bcd7ab0757fb3706fbb
Chen Zhiwei <zhiwei@youya.org>
2023-10-16 19:12:59 +0800

    add mirror support for go and node

--- configure.sh
+++ configure.sh
@@ -3,6 +3,7 @@
 
 set -eux -o pipefail
 
+MIRROR=${MIRROR:-true}
 ROOT_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 
 ## Symlink the config
@@ -33,32 +34,39 @@ function go_install() {
         echo "Go already installed"
         return
     fi
-    local arch kernel version
+    local arch kernel version url
     arch=$(uname -m|sed 's/x86_64/amd64/g')
     kernel=$(uname -s|tr '[:upper:]' '[:lower:]')
-    version=$(curl -sL https://go.dev/VERSION?m=text|head -1)
-    # local url=https://go.dev/dl/$version.$kernel-$arch.tar.gz
-    local url=https://mirrors.aliyun.com/golang/$version.$kernel-$arch.tar.gz
+    if [[ "$version" == "" ]]; then
+        echo "failed to get go version"
+        exit 1
+    fi
+    if [[ $MIRROR == "true" ]]; then
+        version=$(curl -sL https://golang.google.cn/VERSION?m=text|head -1)
+        url=https://golang.google.cn/dl/$version.$kernel-$arch.tar.gz
+    else
+        version=$(curl -sL https://go.dev/VERSION?m=text|head -1)
+        url=https://go.dev/dl/$version.$kernel-$arch.tar.gz
+    fi
 
     curl -kL "$url" | tar -xz -C /tmp/
     mv /tmp/go "$ROOT_PATH/go"
 }
 
-## NodeJS
+## NodeJS installation
 function node_install() {
     if [[ -d "$ROOT_PATH/node" ]]; then
         echo "Node already installed"
         return
     fi
-    local version
-    version=$(curl -sL https://nodejs.org/dist/index.tab | awk 'NR==2 {print $1}')
-    if [[ "$version" == "" ]]; then
-        echo "failed to get node version"
-        exit 1
+    local version curl
+    if [[ $MIRROR == "true" ]]; then
+        version=$(curl -sL https://mirrors.aliyun.com/nodejs-release/index.tab | awk 'NR==2 {print $1}')
+        url=https://mirrors.aliyun.com/nodejs-release/$version/node-$version-linux-x64.tar.xz
+    else
+        version=$(curl -sL https://nodejs.org/dist/index.tab | awk 'NR==2 {print $1}')
+        url=https://nodejs.org/dist/$version/node-$version-linux-x64.tar.xz
     fi
-    local url
-    # url=https://nodejs.org/dist/$VERSION/node-$VERSION-linux-x64.tar.xz
-    url=https://mirrors.aliyun.com/nodejs-release/$version/node-$version-linux-x64.tar.xz
 
     curl -kL "$url" | tar -xJ -C /tmp/
     mv "/tmp/node-$version-linux-x64" "$ROOT_PATH/node"
